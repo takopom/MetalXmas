aa1bb5b09f140394b3128563256632cd065ba5cb
takopom <ymc722@gmail.com>
2016-12-08 00:23:57 +0900

    記述を整理

--- MetalXmas/View/MetalView.swift
+++ MetalXmas/View/MetalView.swift
@@ -12,46 +12,50 @@ import MetalKit
 class MetalView: MTKView {
 
     // MARK: Properties
-    private var vertexBuffer: MTLBuffer!
-    
-    private var piplineState: MTLRenderPipelineState!
-    
     private var commandQueue: MTLCommandQueue!
     
-    private var vertexCount: Int = 0
+    private var piplineState: MTLRenderPipelineState?
     
+    private var texturePiplineState: MTLRenderPipelineState?
+    
+    private var vertexBuffer: MTLBuffer!
+    
+    private var vertexCount: Int = 0
     
     // MARK: Methods
     init(frame: CGRect) {
         super.init(frame: frame, device: MTLCreateSystemDefaultDevice())
         setupMetal()
+        piplineState = makeRenderPipline(vertex: "basic_vertex", fragment: "basic_fragment")
+        setupVertexBuffer()
     }
     
-    required init(coder: NSCoder) {
-        super.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), device: MTLCreateSystemDefaultDevice())
-        setupMetal()
+    required convenience init(coder: NSCoder) {
+        self.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
     }
-    
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-        
+
         render()
     }    
     
     private func setupMetal() {
-        
-        // Metal Device を作る
+        // Metal Deviceを作る
         let device = MTLCreateSystemDefaultDevice()!
         self.device = device
         
-        // Vertex Buffer を作る
+        // Command Queueを作る
+        commandQueue = device.makeCommandQueue()
+    }
+    
+    private func setupVertexBuffer() {
         let vertices = [
-            // 上段
+            // 上段の緑
             Vertex(position: [     0,  0.5, 0, 1], color: [143/255, 195/255, 31/255, 1]),
             Vertex(position: [-0.375, 0.25, 0, 1], color: [ 34/255, 172/255, 56/255, 1]),
             Vertex(position: [ 0.375, 0.25, 0, 1], color: [ 34/255, 172/255, 56/255, 1]),
-            // 下段
+            // 下段の緑
             Vertex(position: [-0.25,  0.25, 0, 1], color: [34/255, 172/255, 56/255, 1]),
             Vertex(position: [ -0.5,     0, 0, 1], color: [ 0/255, 113/255, 48/255, 1]),
             Vertex(position: [    0,     0, 0, 1], color: [ 0/255, 113/255, 48/255, 1]),
@@ -61,7 +65,7 @@ class MetalView: MTKView {
             Vertex(position: [ 0.25,  0.25, 0, 1], color: [34/255, 172/255, 56/255, 1]),
             Vertex(position: [    0,     0, 0, 1], color: [ 0/255, 113/255, 48/255, 1]),
             Vertex(position: [  0.5,     0, 0, 1], color: [ 0/255, 113/255, 48/255, 1]),
-            // 幹
+            // 茶色の幹
             Vertex(position: [-0.125,     0, 0, 1], color: [168/255,  66/255, 0/255, 1]),
             Vertex(position: [-0.125, -0.25, 0, 1], color: [172/255, 106/255, 0/255, 1]),
             Vertex(position: [ 0.125,     0, 0, 1], color: [168/255,  66/255, 0/255, 1]),
@@ -72,12 +76,13 @@ class MetalView: MTKView {
         
         vertexCount = vertices.count
         let vertexBufferLength = vertexCount * MemoryLayout<Vertex>.size
-        vertexBuffer = device.makeBuffer(bytes: UnsafeRawPointer(vertices), length: vertexBufferLength, options: .optionCPUCacheModeWriteCombined)
-        
-        // Vertex shader, Fragment shader を指定する
-        let defaultLibrary = device.newDefaultLibrary()
-        let vertexFunction = defaultLibrary?.makeFunction(name: "basic_vertex")
-        let fragmentFunction = defaultLibrary?.makeFunction(name: "basic_fragment")
+        vertexBuffer = device?.makeBuffer(bytes: UnsafeRawPointer(vertices), length: vertexBufferLength, options: .optionCPUCacheModeWriteCombined)
+    }
+    
+    private func makeRenderPipline(vertex: String, fragment: String) -> MTLRenderPipelineState? {
+        let defaultLibrary = device?.newDefaultLibrary()
+        let vertexFunction = defaultLibrary?.makeFunction(name: vertex)
+        let fragmentFunction = defaultLibrary?.makeFunction(name: fragment)
         
         let piplineDescriptor = MTLRenderPipelineDescriptor()
         piplineDescriptor.vertexFunction = vertexFunction
@@ -95,16 +100,15 @@ class MetalView: MTKView {
         colorAttachment?.destinationRGBBlendFactor = .oneMinusSourceAlpha
         colorAttachment?.destinationAlphaBlendFactor = .oneMinusSourceAlpha
         
-        // Render pipline を作る
+        // Render piplineを作る
+        var piplineState: MTLRenderPipelineState? = nil
         do {
-            piplineState = try device.makeRenderPipelineState(descriptor: piplineDescriptor)
+            piplineState = try device?.makeRenderPipelineState(descriptor: piplineDescriptor)
         } catch {
-            print("unable to compile render pipline state")
-            return
+            print("failed to compile render pipline state.")
         }
         
-        // Command Queue を作る
-        commandQueue = device.makeCommandQueue()
+        return piplineState
     }
     
     private func render() {
@@ -117,12 +121,14 @@ class MetalView: MTKView {
             let colorAttachment = renderPassDescriptor.colorAttachments[0]
             colorAttachment?.texture = drawable.texture
             colorAttachment?.loadAction = .clear
-            colorAttachment?.clearColor = MTLClearColorMake(1, 1, 1, 1)
+            colorAttachment?.clearColor = MTLClearColorMake(0.9, 0.9, 0.9, 1)
             
-            // CommandBufferを生成、三角形を描く
+            // CommandBufferを生成
             let commandBuffer = commandQueue.makeCommandBuffer()
+
+            // ツリーを描く
             let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-            renderEncoder.setRenderPipelineState(piplineState)
+            renderEncoder.setRenderPipelineState(piplineState!)
             renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
             renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertexCount, instanceCount: 1)
             renderEncoder.endEncoding()
